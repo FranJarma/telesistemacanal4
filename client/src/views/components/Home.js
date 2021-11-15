ee1e489f44f36e9bf871e1e3a44dc4c29c5db651
Francisco Jarma <pancho24.1997@gmail.com>
2021-11-14 23:02:29 -0300

    Cambio

--- client/src/views/components/Home.js
+++ client/src/views/components/Home.js
@@ -3,9 +3,23 @@ import React from 'react';
 import Aside from './design/layout/Aside';
 import Footer from './design/layout/Footer';
 import useStyles from './Styles';
+import {XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, VerticalBarSeries} from 'react-vis';
 
 const Home = () => {
     const styles = useStyles();
+    const data = [
+        {x: 0, y: 8},
+        {x: 1, y: 5},
+        {x: 2, y: 4},
+        {x: 3, y: 9},
+        {x: 4, y: 1},
+        {x: 5, y: 7},
+        {x: 6, y: 6},
+        {x: 7, y: 3},
+        {x: 8, y: 2},
+        {x: 9, y: 0}
+    ];
+
     return (
         <>
         <div className="container">
@@ -16,20 +30,29 @@ const Home = () => {
                         <Card className={styles.cartaHome}>
                             <CardContent>
                                 <Typography variant="h1">Ingresos del día</Typography>
+                                <XYPlot height={200} width={200}>
+                                    <VerticalBarSeries data={data} />
+                                </XYPlot>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item lg={4} md={4} xs={12}>
                         <Card className={styles.cartaHome}>
                             <CardContent>
-                                <Typography variant="h1">Cortes del día</Typography>
+                                <Typography variant="h1">Cortes a realizar</Typography>
+                                <XYPlot height={200} width={200}>
+                                    <VerticalBarSeries data={data} />
+                                </XYPlot>
                             </CardContent>
                         </Card>
                     </Grid>
                     <Grid item lg={4} md={4} xs={12}>
                         <Card className={styles.cartaHome}>
                             <CardContent>
-                                <Typography variant="h1">Estadística 3</Typography>
+                                <Typography variant="h1">Instalaciones a realizar</Typography>
+                                <XYPlot height={200} width={200}>
+                                    <VerticalBarSeries data={data} />
+                                </XYPlot>
                             </CardContent>
                         </Card>
                     </Grid>
